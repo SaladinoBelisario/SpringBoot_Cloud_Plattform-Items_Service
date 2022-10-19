8a0a000c3c9d67c7f7756806e2224ec162317381
Saladino Belisario <SaladinoBelisario@protonmail.ch>
2022-10-19 15:20:42 -0500

    feat(application): Add Eureka and Zuul support
    Add Service discovery with Eureka and API Gateway with Zuul

--- src/main/java/dev/saladinobelisario/springboot/app/itemservice/controllers/ItemController.java
+++ src/main/java/dev/saladinobelisario/springboot/app/itemservice/controllers/ItemController.java
@@ -1,6 +1,8 @@
 package dev.saladinobelisario.springboot.app.itemservice.controllers;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import dev.saladinobelisario.springboot.app.itemservice.models.Item;
+import dev.saladinobelisario.springboot.app.itemservice.models.Product;
 import dev.saladinobelisario.springboot.app.itemservice.models.services.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -22,8 +24,23 @@ public class ItemController {
         return itemService.findAll();
     }
 
+    @HystrixCommand(fallbackMethod = "fallbackMethod")
     @GetMapping("/item/{id}/cantidad/{quantity}")
     public Item itemDetail(@PathVariable Long id, @PathVariable Integer quantity){
         return itemService.findByID(id, quantity);
+    }
+
+    public Item fallbackMethod(Long id, Integer quantity){
+        Item item = new Item();
+        Product product = new Product();
+
+        product.setId(id);
+        product.setName("Dummy product");
+        product.setPrice(666.00);
+
+        item.setQuantity(quantity);
+        item.setProduct(product);
+
+        return item;
     }
 }
